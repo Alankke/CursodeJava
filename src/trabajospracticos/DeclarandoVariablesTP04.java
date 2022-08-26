3edd874dbcd32b85e7df34fefdb2f2b19bf5140e
alankke <alaneduardokennedy@gmail.com>
2022-08-26 20:57:24 -0300

    organizando

--- src/trabajospracticos/DeclarandoVariablesTP04.java
+++ src/trabajospracticos/DeclarandoVariablesTP04.java
@@ -1,3 +1,4 @@
+package trabajospracticos;
 public class DeclarandoVariablesTP04 {
     public static void main(String[] args) {
         String nombre = "Alan";
--- src/trabajospracticos/UsandolaAPIconMathTP06.java
+++ src/trabajospracticos/UsandolaAPIconMathTP06.java
@@ -1,3 +1,4 @@
+package trabajospracticos;
 import java.util.Scanner;
 
 public class UsandolaAPIconMathTP06 {
