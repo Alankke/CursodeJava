9c1d7bb23719a8195d363aad6827d8a4ac48e2d6
alankke <alaneduardokennedy@gmail.com>
2022-08-28 22:40:28 -0300

    Trabajo 8

---
+++ src/trabajospracticos/CalcularelpesoTP09.java
@@ -0,0 +1,25 @@
+package trabajospracticos;
+import java.util.Scanner;
+
+public class CalcularelpesoTP09 {
+    public static void main(String[] args) {
+        int cm;
+        String hombre, mujer, genero;
+
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su altura en centimetros (Si es 1,80 en metros entonces ingrese 180 en centimetros): ");
+        cm = leer.nextInt();
+        System.out.println("Ahora ingrese su género (hombre o mujer): ");
+        leer.nextLine();
+        genero = leer.nextLine();
+        if (genero.equals("hombre")){
+            System.out.println("Su peso ideal es: " + (cm - 110) + "kg");
+        }
+        else if (genero.equals("mujer")) {
+            System.out.println("Su peso ideal es: " + (cm - 120) + "kg");
+        }
+        else {
+            System.out.println("Ingresó incorrectamente su genero o centimetros");
+            }
+        }
+    }
