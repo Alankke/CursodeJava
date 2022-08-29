8e26a389a7c4d8968be8a3b95ea285fb2feb38da
alankke <alaneduardokennedy@gmail.com>
2022-08-28 21:56:33 -0300

    subida nueva

---
+++ src/trabajospracticos/RaizScanner.java
@@ -0,0 +1,14 @@
+package trabajospracticos;
+import java.util.Scanner;
+
+public class RaizScanner {
+    public static void main(String[] args) {
+        int nroIngresado;
+        double resul;
+        System.out.println("Ingrese un numero");
+        Scanner leer = new Scanner(System.in);
+        nroIngresado = leer.nextInt();
+        resul = Math.sqrt(nroIngresado);
+        System.out.println("La raiz del numero ingresado es :" + resul);
+    }
+}
